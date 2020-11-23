3044147c3b8662e206b8c3526f29438eef7d87ea
Hendriyana <74785882+Hendriyana78@users.noreply.github.com>
2020-11-23 09:16:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM Selection
+// document.getElementById() -> element
+const judul = document.getElementById('judul');
+judul.style.color='white';
+
+// document.getElementsByTagName()
+const p = document.getElementsByTagName('p');
+
+const nav = document.getElementsByTagName('nav');
+nav[0].style.backgroundColor='#00a2c6';
